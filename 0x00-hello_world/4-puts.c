de62e426b9635c75183122074e9239e7dc426441
muhdrawayya <muhdusman@gmail.com>
2021-06-21 15:04:13 -0700

    puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ *main - entry point
+ *
+ *return: 0 after printing the function
+ */
+init main(void)
+{
+puts("\" Programming is like building a multilingual puzzle");
+return (0);
+}
+
